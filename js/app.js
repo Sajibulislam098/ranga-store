4043769268c82176ce4d6c74fd39f022b71ed964
Sajibulislam098 <sajibul.islam098@gmail.com>
2021-09-13 22:42:57 +0600

    card  color add

--- js/app.js
+++ js/app.js
@@ -11,17 +11,20 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class=" single-product">
       <div>
     <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
+      </div> 
+      <div class="product-body   ">
+      <h5>${product.title}</h5>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <h3>Rating: ${product.rating.rate} </h3>
+      <h3>Price: $ ${product.price}</h3>
+      <h5>Rating: ${product.rating.rate} </h5>
       <p>(Total Rating Count: ${product.rating.count}) </p>
+      <div class="display ">
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button>
+      </div></div>
       </div>
         
       `;
